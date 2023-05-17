875b319aab291f293cbd9f41a27b992e4b4ce242
pablo-figueroa <pablo.figueroa.7once@gmail.com>
2023-05-17 13:39:55 -0300

    Tema 6 - Ejercicio 5 - Remake

--- T6_Ej05.pas
+++ T6_Ej05.pas
@@ -1,5 +1,6 @@
 (*
 ----------------------------------------- EJERCICIO 5 -----------------------------------------
+REMAKE
 
 Leer de un archivo de texto sobre tres arreglos los datos de un conjunto de autos, por cada uno:
     - Patente
@@ -13,27 +14,6 @@ calcular:
     c. Para un rango de años dado [Año1, Año2] precio promedio de los autos en dicho rango (puede
 no existir)
 
-Ejemplo:
-    AAA111	2017	4000,00
-    BBB222	2017	4500,00
-    CCC333	2017	5000,00
-    DDD444	2017	5500,00
-    EEE555	2017	6000,00
-    AAA111	2018	3960,00
-    BBB222	2018	4455,00
-    CCC333	2018	4950,00
-    DDD444	2018	5445,00
-    EEE555	2018	5940,00
-    AAA111	2019	3920,40
-    ...
-
-    a. 2017          -> Precio Mínimo : 4000.00
-       2018          -> Precio Mínimo : 3960.00
-
-    b. 5500          -> Cantidad de Vehiculos por debajo 5500 : 19
-
-    c. {2017 ; 2018] -> Promedio : 4975.00
-
 *)
 
 program T6_Ej05;
@@ -41,6 +21,7 @@ program T6_Ej05;
 uses crt;
 
 type
+    TV_Str6       = array[1..100] of string[6];
     TV_100_UInt16 = array[1..100] of UInt16; { word }
     TV_100_Real   = array[1..100] of Real;
 
@@ -48,6 +29,7 @@ type
 { GENERAR VECTORES ---------------------------------------------------------------------------}
 procedure GenerarVectores
     (
+        var VectorPaten  : TV_Str6;
         var VectorAnio   : TV_100_UInt16;
         var VectorPrecio : TV_100_Real;
         var N            : byte
@@ -59,22 +41,19 @@ var
     pr : real;      { Para leer el precio  }
 
     arch : text;
-    i    : byte;
 
 begin
-    i := 0;
     N := 0;
     Assign(arch, 'T6_Ej05.txt');
     Reset(arch);
-    repeat
-        ReadLn(arch, pa,an,pr);
-        (* Write(pa,' ',an,' ',pr:0:2); *)
-        (* WriteLn; *)
-        i := i + 1;
-        VectorAnio[i]   := an;
-        VectorPrecio[i] := pr;
+    while not eof(arch) do
+        begin
         N := N + 1;
-    until eof(arch);
+        ReadLn(arch, pa,an,pr);
+        VectorPaten[N]  := pa;
+        VectorAnio[N]   := an;
+        VectorPrecio[N] := pr;
+        end; {while}
 end; {procedure}
 
 
@@ -97,7 +76,6 @@ begin
         WriteLn(' * | Fin del Programa.');
         WriteLn;
         Write('---> Ingrese Opción: ');
-        (* ReadLn(Op); *)
         Op := ReadKey;
         if Op = '*' then
             halt;
@@ -125,6 +103,7 @@ begin
         begin
         if AnioMin > VectorAnio[i] then
             AnioMin := VectorAnio[i];
+        
         if AnioMax < VectorAnio[i] then
             AnioMax := VectorAnio[i];
         end;{for}
@@ -141,193 +120,214 @@ begin
 end;{procedure Separador}
 
 
-{ a. Para un año dado, precio mínimo (puede no existir) --------------------------------------}
-procedure PrecioMinimo
+{ a. PARA UN AÑO DADO, PRECIO MINIMO (puede no existir) --------------------------------------}
+function PrecioMinimo 
     (
         VectorAnio   : TV_100_UInt16;
         VectorPrecio : TV_100_Real;
-        N            : byte;
-        AnioMin      : word;
-        AnioMax      : word
-    );
+        N            : UInt8;  {byte}
+        Anio         : UInt16  {word}
+    ):Real;
 
 var
-    i         : byte;
-    anio      : word;
-    PrecioMin : real;
-    S         : char;
+    i         : UInt8;  {byte}
+    PrecioMin : Real;
 
 begin
-repeat
     PrecioMin := 9999999;
-    repeat
-        gotoxy(1,1);
-        ClrScr;
-        WriteLn('Ingresar el AÑO del cual desea conocer el Precio Minimo: ');
-        ClrEol;
-        Write(' Desde: ',AnioMin,' | Hasta: ',AnioMax,' ---> ');
-        ReadLn(Anio);
-    until (Anio >= AnioMin) and (Anio <= AnioMax);
-
-    WriteLn;
-
-    for i:=1 to N do
-        begin
+    for i:=1 to N do begin
         if (anio = VectorAnio[i]) and (PrecioMin > VectorPrecio[i]) then
             PrecioMin := VectorPrecio[i];
-        end;{for}
-
-    if PrecioMin = 9999999 then
-        begin
-        TextColor(yellow);
-        WriteLn(' Año no encontrado en el rango -> No hay precio mínimo');
-        TextColor(white);
-        end
-    else
-        WriteLn(' El Precio Mínimo para el año ',Anio,': $',PrecioMin:0:2);
-
-    WriteLn;
-
-    Separador;
-
-    TextColor(red);
-    Write('Enter para reiniciar | S -> Salir ');
-    TextColor(white);
-    S := ReadKey;
-    S := UpCase(S);
-
-until (S = 'S');
+    end;{for}
+    PrecioMinimo := PrecioMin;
 end; {procedure}
 
 
-{ b. Para un precio dado cantidad de vehículos por debajo de dicho valor ---------------------}
-procedure CantidadVehiculos
+{ b. PARA UN PRECIO DADO CANTIDAD DE VEHICULOS POR DEBAJO DE DICHO VALOR ---------------------}
+function CantidadVehiculos
     (
         VectorPrecio : TV_100_Real;
-        N            : byte
-    );
+        N            : UInt8; {byte}
+        Precio       : Real
+    ):Byte;
 
 var
-    i         : byte;
-    ContVeh   : byte;
-    Precio    : real;
-    S         : char;
+    i       : UInt8; {byte}
+    ContVeh : UInt8; {byte} {Dado que el array es de 100}
 
 begin
-repeat
     ContVeh := 0;
-    ClrScr;
-    Write('Ingrese Precio: ');
-    ReadLn(Precio);
-
-    for i:=1 to N do
-        begin
+    for i:=1 to N do begin
         if VectorPrecio[i] < Precio then
             ContVeh := ContVeh + 1;
-        end; {for}
-    WriteLn;
-    WriteLn('Cantidad de Vehículos es: ',ContVeh);
-
-    Separador;
-
-    TextColor(red);
-    Write('Enter para reiniciar | S -> Salir ');
-    TextColor(white);
-    S := ReadKey;
-    S := UpCase(S);
-
-until (S = 'S');
+    end; {for}
+    CantidadVehiculos := ContVeh;
 end; {procedure}
 
 
 { c. Para un rango de años dado [Año1, Año2] precio promedio de los autos en dicho rango -----}
-procedure PrecioPromedio
+function PrecioPromedio
     (
         VectorAnio   : TV_100_UInt16;
         VectorPrecio : TV_100_Real;
         N            : byte;
-        AnioMin      : word;
-        AnioMax      : word
-    );
+        AnioI        : UInt16; {word}
+        AnioF        : UInt16  {word}
+    ):Real;
 
 var
-    i          : byte;
-    AcumPrecio : real;
-    ContVeh    : byte;
-    AnioI      : word;
-    AnioF      : word;
-    S          : char;
-
+    i          : UInt8;  {byte}
+    ContVeh    : UInt8;  {byte}
+    AcumPrecio : Real;
+    
 begin
-repeat
-    AcumPrecio := 0;
     ContVeh    := 0;
-    ClrScr;
-    Writeln('RANGO DE AÑOS: { ',AnioMin,' ; ',AnioMax,' }');
-    Writeln(' Ingrese SUBRANGO');
-
-    repeat
-        gotoxy(1,3);
-        ClrEol;
-        Write('  Año Inicio ---> ');
-        ReadLn(AnioI);
-    until (AnioI >= AnioMin) and (AnioI <= AnioMax);
-
-    repeat
-        gotoxy(1,4);
-        ClrEol;
-        Write('  Año Fin    ---> ');
-        ReadLn(AnioF);
-    until (AnioF >= AnioI) and (AnioF <= AnioMax);
-
+    AcumPrecio := 0;
     for i:=1 to N do
         begin
-        if (VectorAnio[i] >= AnioI) and (VectorAnio[i] <= AnioF) then
-            begin
+        if (VectorAnio[i] >= AnioI) and (VectorAnio[i] <= AnioF) then begin
             AcumPrecio := AcumPrecio + VectorPrecio[i];
             ContVeh := ContVeh + 1;
-            end;{if}
-        end;{for}
-
-    WriteLn;
-
-    if ContVeh <> 0 then
-        WriteLn('El Precio Promedio es: $',(AcumPrecio/ContVeh):0:2)
-    else
-        WriteLn('No existen datos para calcular');
-
-    Separador;
-
-    TextColor(red);
-    Write('Enter para reiniciar | S -> Salir ');
-    TextColor(white);
-    S := ReadKey;
-    S := UpCase(S);
-
-until (S = 'S');
+        end;{if}
+    end;{for}
+    if ContVeh > 0 then
+        PrecioPromedio := AcumPrecio/ContVeh;
+    
+    
+    
+    
 end; {procedure}
 
 
 { PROGRAMA PRINCIPAL -------------------------------------------------------------------------}
 
 var
+    VectorPaten  : TV_Str6;
     VectorAnio   : TV_100_UInt16;
     VectorPrecio : TV_100_Real;
-    N            : byte;
-    AnioMin      : word;
-    AnioMax      : word;
+    N            : UInt8;  {byte}
+
+    AnioMin      : UInt16; {word}
+    AnioMax      : UInt16; {word}
+
+    Anio         : UInt16; {word}
+    PrecioMin    : Real;
+    
+    Precio       : Real;
+    CantVeh      : UInt8;  {byte}
+    
+    AnioI        : UInt16; {word} {Año Inicial}
+    AnioF        : UInt16; {word} {Año Final  }
+    PrecioProm   : Real;
+    
     Op           : char;
+    S            : char;
 
 begin
-    GenerarVectores(VectorAnio,VectorPrecio,N);
+    GenerarVectores(VectorPaten,VectorAnio,VectorPrecio,N);
     AnioMinMax(VectorAnio,N,AnioMin,AnioMax);
 
-repeat
-    Menu(Op);
-    case Op of
-        'A' : PrecioMinimo(VectorAnio,VectorPrecio,N,AnioMin,AnioMax);
-        'B' : CantidadVehiculos(VectorPrecio,N);
-        'C' : PrecioPromedio(VectorAnio,VectorPrecio,N,AnioMin,AnioMax);
-    end;
-until false; {Sale y Fin de Programa con una opción dentro del procedimiento Menu : * -> halt;}
+    repeat
+        Menu(Op);
+        case Op of
+        'A' : 
+            begin
+                repeat
+                    repeat
+                        ClrScr;
+                        WriteLn('Ingresar el AÑO del cual desea conocer el Precio Minimo: ');
+                        Write(' Desde: ',AnioMin,' | Hasta: ',AnioMax,' ---> ');
+                        ReadLn(Anio);
+                    until (Anio >= AnioMin) and (Anio <= AnioMax);
+            
+                    PrecioMin := PrecioMinimo(VectorAnio,VectorPrecio,N,Anio);
+        
+                    if PrecioMin = 9999999 then begin
+                        TextColor(yellow);
+                        WriteLn(' Año no encontrado en el rango -> No hay precio mínimo');
+                        TextColor(white);
+                        end
+                    else
+                        WriteLn(' El Precio Mínimo para el año ',Anio,': $',PrecioMin:0:2);
+        
+                    WriteLn;
+        
+                    Separador;
+        
+                    TextColor(red);
+                    Write('Enter para reiniciar | S -> Salir ');
+                    TextColor(white);
+                    S := ReadKey;
+                    S := UpCase(S);
+                
+                until (S = 'S');
+            end; {'A'}
+            
+                    
+        'B' : 
+            begin
+                repeat
+                    ClrScr;
+                    Write('Ingrese Precio: ');
+                    ReadLn(Precio);
+                    
+                    CantVeh := CantidadVehiculos(VectorPrecio,N,Precio);
+        
+                    if CantVeh > 0 then 
+                        WriteLn('Cantidad de Vehículos es: ',CantVeh)
+                    else 
+                        WriteLn('No se encontraron vehículos');
+                    
+                    Separador;
+        
+                    TextColor(red);
+                    Write('Enter para reiniciar | S -> Salir ');
+                    TextColor(white);
+                    S := ReadKey;
+                    S := UpCase(S);
+                until (S = 'S');
+            end; {'B'}
+            
+        'C' : 
+            begin
+                repeat
+                    ClrScr;
+                    Writeln('RANGO DE AÑOS: { ',AnioMin,' ; ',AnioMax,' }');
+                    Writeln(' Ingrese SUBRANGO');
+                
+                    repeat
+                        gotoxy(1,3);
+                        ClrEol;
+                        Write('  Año Inicio ---> ');
+                        ReadLn(AnioI);
+                    until (AnioI >= AnioMin) and (AnioI <= AnioMax);
+                
+                    repeat
+                        gotoxy(1,4);
+                        ClrEol;
+                        Write('  Año Fin    ---> ');
+                        ReadLn(AnioF);
+                    until (AnioF >= AnioI) and (AnioF <= AnioMax);
+            
+                    PrecioProm := PrecioPromedio(VectorAnio,VectorPrecio,N,AnioI,AnioF);
+        
+                    WriteLn;
+        
+                    if PrecioProm > 0 then
+                        WriteLn('El Precio Promedio es: $',PrecioProm:0:2)
+                    else
+                        WriteLn('No existen datos para calcular');
+                
+                    Separador;
+                
+                    TextColor(red);
+                    Write('Enter para reiniciar | S -> Salir ');
+                    TextColor(white);
+                    S := ReadKey;
+                    S := UpCase(S);
+                
+                until (S = 'S');
+            end; {'C'}
+        end; {case}
+    until false; {Sale y Fin de Programa con una opción dentro del procedimiento Menu : * -> halt;}
 end.
